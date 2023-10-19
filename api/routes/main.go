1fdb98882219dacf63bc8d00efff9a44531002fe
Wanderlei Pereira <wanderlei2583@gmail.com>
2023-10-19 13:13:20 -0300

    feat: added route codes

---
+++ api/routes/main.go
@@ -0,0 +1,18 @@
+package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/wander2583/api-go-rest/api/controllers"
+)
+
+func AppRoutes(router *gin.Engine) *gin.RouterGroup {
+
+	tweetController := controllers.NewTweetController()
+
+	v1 := router.Group("/v1")
+	{
+		v1.GET("/tweets", tweetController.FindAll)
+	}
+
+	return v1
+}
