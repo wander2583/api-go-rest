ead7c83da0dfdc824bf441750b42a8b90e0b5e31
Wanderlei Pereira <wanderlei2583@gmail.com>
2023-10-19 13:28:54 -0300

    feat: adding post route

--- api/routes/main.go
+++ api/routes/main.go
@@ -12,6 +12,7 @@ func AppRoutes(router *gin.Engine) *gin.RouterGroup {
 	v1 := router.Group("/v1")
 	{
 		v1.GET("/tweets", tweetController.FindAll)
+		v1.POST("/tweets", tweetController.Create)
 	}
 
 	return v1
