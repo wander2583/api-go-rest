ee65d4fcae330d36cb22b19745249a9f55c34773
Wanderlei Pereira <wanderlei2583@gmail.com>
2023-10-19 13:39:34 -0300

    feat: creating or deleting

--- api/controllers/TweetController.go
+++ api/controllers/TweetController.go
@@ -30,3 +30,17 @@ func (t *tweetController) Create(ctx *gin.Context) {
 
 	ctx.JSON(http.StatusOK, t.tweets)
 }
+
+func (t *tweetController) Delete(ctx *gin.Context) {
+	id := ctx.Param("id")
+
+	for idx, tweet := range t.tweets {
+		if tweet.ID == id {
+			t.tweets = append(t.tweets[0:idx], t.tweets[idx+1:]...)
+			return
+		}
+	}
+	ctx.JSON(http.StatusNotFound, gin.H{
+		"error": "Tweet not found!",
+	})
+}
