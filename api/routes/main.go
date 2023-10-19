2f0a167562c1417fb32d186795178099e1c35af9
Wanderlei Pereira <wanderlei2583@gmail.com>
2023-10-19 19:34:21 -0300

    feat: adding delete

--- api/routes/main.go
+++ api/routes/main.go
@@ -12,8 +12,8 @@ func AppRoutes(router *gin.Engine) *gin.RouterGroup {
 	v1 := router.Group("/v1")
 	{
 		v1.GET("/tweets", tweetController.FindAll)
-		v1.POST("/tweets", tweetController.Create)
-		v1.DELETE("/tweets", tweetController.Delete)
+		v1.POST("/tweet", tweetController.Create)
+		v1.DELETE("/tweet/:id", tweetController.Delete)
 	}
 
 	return v1
